e8fbefde892c82e38051eeba05144c2fca831c57
Анатолий <reloader2003@gmail.com>
2020-03-21 11:45:47 +0700

    Дерево, суммы, много данных, асинхронность

---
+++ TreeViewer/TreeViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeViewer.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            LoadedCommand = new DelegateCommand(InitTreeAsync);
+
+            Tree = new List<TreeNodeViewModel>
+            {
+                new TreeNodeViewModel
+                {
+                    Name = "root"
+                }
+            };
+        }
+
+        private async void InitTreeAsync()
+        {
+            const int deep = 3;
+            const int childrenCount = 5;
+
+            Func<TreeNodeViewModel, int, Func<string>, Task> strategyDelegate = FeelNodeAsync; // 18 sec
+            //Func<TreeNodeViewModel, int, Func<string>, Task> strategyDelegate = FeelNode2Async; // 18 sec
+
+            await strategyDelegate(Tree[0], childrenCount, () => "item");
+            var currentNodes = Tree.SelectMany(x => x.Children);
+
+            foreach (var child in Tree)
+            {
+                await strategyDelegate(child, childrenCount, null);
+            }
+
+            for (var i = 0; i < deep; i++)
+            {
+                foreach(var child in currentNodes)
+                {
+                    await strategyDelegate(child, childrenCount, null);
+                }
+                currentNodes = currentNodes.SelectMany(x => x.Children);
+            }
+        }
+
+        private async Task FeelNodeAsync(TreeNodeViewModel currentNode, int childrenCount, Func<string> templateSelector = null)
+        {
+            var nodes = await CreateNodesAsync(childrenCount, templateSelector?.Invoke() ?? currentNode.Name);
+            foreach (var node in nodes)
+            {
+                currentNode.Children.Add(node);
+            }
+        }
+
+        private async Task FeelNode2Async(TreeNodeViewModel currentNode, int childrenCount, Func<string> templateSelector = null)
+        {
+            await foreach (var node in CreateNodes2Async(childrenCount, templateSelector?.Invoke() ?? currentNode.Name))
+            {
+                currentNode.Children.Add(node);
+            }
+        }
+
+        private Task<IEnumerable<TreeNodeViewModel>> CreateNodesAsync(int count, string nameTemplate)
+        {
+            return Task.Run(() =>
+            {
+                var result = new List<TreeNodeViewModel>();
+                for (var i = 0; i < count; i++)
+                {
+                    var rnd = new Random((int)DateTime.Now.Ticks);
+                    result.Add(new TreeNodeViewModel
+                    {
+                        Name = $"{nameTemplate}_{i}",
+                        Value = rnd.Next(1, 10),
+                        Value2 = rnd.Next(1,10)
+                    });
+                }
+                return result.AsEnumerable();
+            });
+        }
+
+        private async IAsyncEnumerable<TreeNodeViewModel> CreateNodes2Async(int count, string nameTemplate)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var node = await Task.Run(() =>
+                {
+                    var rnd = new Random((int)DateTime.Now.Ticks);
+                    return new TreeNodeViewModel
+                    {
+                        Name = $"{nameTemplate}_{i}",
+                        Value = rnd.Next(1, 10),
+                        Value2 = rnd.Next(1, 10)
+                    };
+                });
+
+                yield return node;
+            }
+        }
+
+        public DelegateCommand LoadedCommand { get; }
+
+        public List<TreeNodeViewModel> Tree { get; }
+    }
+}
---
+++ TreeViewer/TreeViewer/ViewModels/TreeNodeViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TreeViewer.ViewModels
+{
+    public class TreeNodeViewModel : BindableBase
+    {
+        private string _name;
+        private int? _value;
+        private int? _value2;
+
+        public string Name { 
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        public int? Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+
+        public int? Value2
+        {
+            get => _value2;
+            set => SetProperty(ref _value2, value);
+        }
+
+        public int? Average => _value is null && _value2 is null
+            ? default
+            : ((_value ?? 0) + (_value2 ?? 0)) / 2;
+
+        public bool HasChildren => Children.Count > 0;
+
+        public TreeNodeViewModel()
+        {
+            Children = new ObservableCollection<TreeNodeViewModel>();
+            Children.CollectionChanged += Children_CollectionChanged;
+
+            PropertyChanged += Self_PropertyChanged;
+        }
+
+        private void Self_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Value) || e.PropertyName == nameof(Value2))
+                RaisePropertyChanged(nameof(Average));
+        }
+
+        private void Children_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (var item in e.NewItems.Cast<TreeNodeViewModel>())
+                    Subscribe(item);
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (var item in e.OldItems.Cast<TreeNodeViewModel>())
+                    Unsubscribe(item);
+            }
+            Calculate();
+        }
+
+        private void Subscribe(TreeNodeViewModel item)
+        {
+            item.PropertyChanged += Children_PropertyChanged;
+        }
+
+        private void Unsubscribe(TreeNodeViewModel item)
+        {
+            item.PropertyChanged -= Children_PropertyChanged;
+        }
+
+        private void Children_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Value) || e.PropertyName == nameof(Value2))
+                Calculate();
+        }
+
+        private void Calculate()
+        {
+            Value = Children.Any(x => x.Value.HasValue) ? Children.Sum(x => x.Value) : null;
+            Value2 = Children.Any(x => x.Value2.HasValue) ? Children.Sum(x => x.Value2) : null;
+        }
+
+        public ObservableCollection<TreeNodeViewModel> Children { get; }
+    }
+}
